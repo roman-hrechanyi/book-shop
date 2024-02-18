8d731f717f5370d0053273f44acf7674f7d54150
Roman <grechoroma@gmail.com>
2024-02-18 19:11:37 +0200

    BookDto, BookController

--- src/main/java/com/grech/service/BookService.java
+++ src/main/java/com/grech/service/BookService.java
@@ -1,10 +1,13 @@
 package com.grech.service;
 
-import com.grech.model.Book;
+import com.grech.dto.BookDto;
+import com.grech.dto.CreateBookRequestDto;
 import java.util.List;
 
 public interface BookService {
-    Book save(Book book);
+    BookDto save(CreateBookRequestDto requestDto);
 
-    List<Book> findAll();
+    List<BookDto> findAll();
+
+    BookDto findById(Long id);
 }
